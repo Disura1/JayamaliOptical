81dea8a8b1f63f6f9f4b2068818a30be16c6b548
Disura Sandaruwan <disurasandaruwan0@gmail.com>
2026-03-15 10:37:47 +0530

    Build Dashboard with Statistics

--- JayamaliOptical.Web/Areas/Admin/Controllers/HomeController.cs
+++ JayamaliOptical.Web/Areas/Admin/Controllers/HomeController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using JayamaliOptical.Web.Models;
+using JayamaliOptical.Web.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace JayamaliOptical.Web.Areas.Admin.Controllers
 {
@@ -7,9 +11,28 @@ namespace JayamaliOptical.Web.Areas.Admin.Controllers
     [Authorize]
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var model = new DashboardViewModel
+            {
+                TotalCustomers = await _context.Users
+                    .Where(u => !u.Email!.EndsWith("@jayamalioptical.com"))
+                    .CountAsync(),
+
+                TotalOrders = 0,
+                TotalProducts = 0,
+                TodayAppointments = 0,
+                TotalRevenue = 0
+            };
+
+            return View(model);
         }
     }
 }
---
+++ JayamaliOptical.Web/Models/DashboardViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace JayamaliOptical.Web.Models
+{
+    public class DashboardViewModel
+    {
+        public int TotalCustomers { get; set; }
+        public int TotalOrders { get; set; }
+        public int TotalProducts { get; set; }
+        public int TodayAppointments { get; set; }
+        public decimal TotalRevenue { get; set; }
+    }
+}
