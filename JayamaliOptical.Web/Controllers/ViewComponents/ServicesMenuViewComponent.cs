a03369e4b4249b126d6dd6d7d1a16b6bb13466f8
Disura Sandaruwan <disurasandaruwan0@gmail.com>
2026-03-18 00:46:30 +0530

    Updated service navlink dropdown to manage with database.

---
+++ JayamaliOptical.Web/Controllers/ViewComponents/ServicesMenuViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using JayamaliOptical.Web.Data;
+using JayamaliOptical.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace JayamaliOptical.Web.ViewComponents
+{
+    public class ServicesMenuViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ServicesMenuViewComponent(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var services = await _context.Services
+                .Where(s => s.IsActive)
+                .OrderBy(s => s.DisplayOrder)
+                .Take(10) // Limit to 10 services in dropdown
+                .ToListAsync();
+
+            return View(services);
+        }
+    }
+}
